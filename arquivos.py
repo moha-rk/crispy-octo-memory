f907d6919677ee6ac7bcd6da107b77d24624d5eb
Mohamad Rkein <mohamad_rkein@usp.br>
2021-06-27 15:35:14 +0300

    Manipulação do arquivo e listas de comentários/descontos

--- arquivos.py
+++ arquivos.py
@@ -1,6 +1,7 @@
 #Arquivo com funções relacionadas à abertura, leitura e manipulação de arquivos
 
 from os import listdir
+from typing import List
 
 def conteudo_dirertorio(path: str) -> list:
     """Recebe um caminho e devolve a lista de nomes de arquivos e pastas dentro desse caminho"""
@@ -23,6 +24,7 @@ def nomes_alunos_edisciplinas(path: str) -> list:
 
 nome_do_trabalho = ''
 lista_alunos = []
+lista_comentarios = []
 
 def set_nome_trabalho(nome: str) -> int:
     """Seta o nome do trabalho. Devolve 1 em caso de suesso, 
@@ -37,6 +39,8 @@ def set_nome_trabalho(nome: str) -> int:
     global nome_do_trabalho
     nome_do_trabalho = nome
 
+    return 1
+
 def get_nome_trabalho() -> str:
     return nome_do_trabalho
 
@@ -45,6 +49,9 @@ def nome_pasta() -> str:
 
 def nome_arquivo_chamada() -> str:
     return f'{nome_pasta()}/lista_chamada.txt'
+
+def nome_arquivo_coments() -> str:
+    return f'{nome_pasta()}/comentarios.txt'
 
 def gera_lista_alunos(path_trabalhos: str, novo_trabalho: int) -> None:
     """Essa função deve ser chamada na criação de um novo trabalho (1) 
@@ -87,18 +94,18 @@ def le_lista_alunos_arquivo() -> None:
 def adiciona_desconto(aluno: str, desconto: int) -> None:
     """Essa função recebe o nome do aluno a levar o desconto, e também recebe o número
        do desconto, alterando em memória e no arquivo. Trata adição de desconto repetido"""
-    
-    for a in lista_alunos:
-        if a[0] == aluno:
-            if not (desconto in a[1]):
-                a[1].append(desconto)
-                a[1].sort()
+    global lista_alunos
+    for i in range (len(lista_alunos)):
+        if lista_alunos[i][0] == aluno:
+            if not (desconto in lista_alunos[i][1]):
+                lista_alunos[i][1].append(desconto)
+                lista_alunos[i][1].sort()
                 #Atualizar a cada desconto feito.
                 #Ineficiente, mas mais fácil para protótipo inicial
-                atualiza_arquivo_chamada()
+                atualiza_arquivo_chamada(lista_alunos)
             break
 
-def atualiza_arquivo_chamada() -> None:
+def atualiza_arquivo_chamada(lista_alunos: list) -> None:
     """Essa função deve ser chamada com a list lista_alunos atualizada
        e então escreverá seu conteúdo no arquivo"""
     with open(nome_arquivo_chamada(), 'w') as f:
@@ -108,4 +115,68 @@ def atualiza_arquivo_chamada() -> None:
                 str_descontos += f'{desconto} '
             str_descontos = str_descontos[:-1] #remove ultimo espaço em branco
             entry = f'{lista_alunos[i][0]}{str_descontos}\n'
+            f.write(entry)
+
+def le_comentarios_arquivo() -> list:
+    """Devolve uma lista de comentários, onde o índice do comentário equivale ao seu código"""
+    global lista_comentarios
+    lista_comentarios = []
+    try:
+        with open(nome_arquivo_coments(), 'r') as f:
+            for line in f:
+                line = line.strip('\n')
+                entry = []
+                if line[-1] == '.':
+                    entry = [line, None]
+                else:
+                    size_desconto = 1
+                    while size_desconto < len(line) and line[-size_desconto] != '(':
+                        size_desconto += 1
+                    desconto = int(line[len(line)-size_desconto+1: -1])
+                    entry = [line[:-size_desconto-1], desconto]
+                lista_comentarios.append(entry)
+
+    except FileNotFoundError:
+        open(nome_arquivo_coments(), 'w')
+    
+    return lista_comentarios
+
+def adiciona_comentario(comentario: str, desconto: int) -> list:
+    global lista_comentarios
+
+    for coment in lista_comentarios:
+        if coment[0] == comentario:
+            print("Comentário já existente")
+            return
+    
+    if desconto == 0:
+        desconto = None
+    lista_comentarios.append([comentario, desconto])
+    atualiza_arquivo_comentario()
+
+
+def atualiza_desconto_comentario(comentario: str, desconto: int) -> list:
+    """Recebe um comentario e o novo desconto atrelado a este comentario,
+       atualiza a lista global e a devolve"""
+    global lista_comentarios
+    if desconto > 0:
+        desconto = -desconto
+    
+    for i in range (len(lista_comentarios)):
+        if lista_comentarios[i][0] == comentario:
+            lista_comentarios[i][1] = desconto
+            #Atualizar a cada desconto feito.
+            #Ineficiente, mas mais fácil para protótipo inicial
+            atualiza_arquivo_comentario(lista_comentarios)
+            break
+
+def atualiza_arquivo_comentario() -> None:
+    """Essa função deve ser chamada com a list lista_comentarios atualizada
+       e então escreverá seu conteúdo no arquivo"""
+    with open(nome_arquivo_coments(), 'w') as f:
+        for i in range (len(lista_comentarios)):
+            if lista_comentarios[i][1]:
+                entry = f'{lista_comentarios[i][0]} ({lista_comentarios[i][1]})\n'
+            else:
+                entry = f'{lista_comentarios[i][0]}\n'
             f.write(entry)
